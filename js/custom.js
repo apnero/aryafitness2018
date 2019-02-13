9256f23bc7b5f17ec0bc5db30683b5c5194f06e0
CommitUser <CommitUser@nerodm.com>
2019-02-12 22:53:27 -0500

    more

--- js/custom.js
+++ js/custom.js
@@ -618,3 +618,29 @@ jQuery(document).ready(function($) {
     })
     $('.contact-column').matchHeight();
 });
+
+
+// lightbox
+window.document.onkeydown = function(e) {
+  if (!e) {
+    e = event;
+  }
+  if (e.keyCode == 27) {
+    lightbox_close();
+  }
+}
+
+function lightbox_open() {
+  var lightBoxVideo = document.getElementById("AryaAboutPageVideo");
+  window.scrollTo(0, 0);
+  document.getElementById('light').style.display = 'block';
+  document.getElementById('fade').style.display = 'block';
+  lightBoxVideo.play();
+}
+
+function lightbox_close() {
+  var lightBoxVideo = document.getElementById("AryaAboutPageVideo");
+  document.getElementById('light').style.display = 'none';
+  document.getElementById('fade').style.display = 'none';
+  lightBoxVideo.pause();
+}
